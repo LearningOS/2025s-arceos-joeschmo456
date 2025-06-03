554fce23f8067ad24c530b4d7ca76783d893bbec
joeschmo456 <imzhangxiangbo@gmail.com>
2025-06-03 23:05:34 +0800

    finish last

--- arceos/exercises/simple_hv/src/main.rs
+++ arceos/exercises/simple_hv/src/main.rs
@@ -101,18 +101,24 @@ fn vmexit_handler(ctx: &mut VmCpuRegisters) -> bool {
             }
         }
         Trap::Exception(Exception::IllegalInstruction) => {
-            panic!(
+            ax_println!(
                 "Bad instruction: {:#x} sepc: {:#x}",
                 stval::read(),
                 ctx.guest_regs.sepc
             );
+            ctx.guest_regs.gprs.set_reg(A1, 0x1234);
+            ctx.guest_regs.sepc += 4;
+            return false;
         }
         Trap::Exception(Exception::LoadGuestPageFault) => {
-            panic!(
+            ax_println!(
                 "LoadGuestPageFault: stval{:#x} sepc: {:#x}",
                 stval::read(),
                 ctx.guest_regs.sepc
             );
+            ctx.guest_regs.sepc += 4;
+            ctx.guest_regs.gprs.set_reg(A0, 0x6688);
+            return false;
         }
         _ => {
             panic!(
